dc377fd01c6ab9d1661f2385a6a51d0c792577ee
taeyeongs <kjpbb@hanmail.net>
2022-11-11 17:14:10 +0900

    up

---
+++ src/test/ch06/Car.java
@@ -0,0 +1,31 @@
+package test.ch06;
+
+public class Car {
+	// 1. 필드: 객체의 데이터를 저장
+	String company = "현대 자동화무장강도";
+	String model;
+	String color;
+	int maxSpeed;
+//	boolean start; //시동여부
+	int speed;
+
+	Car(String model) {
+//		this.model = model;
+		this(model, "은색", 250);
+	}
+
+	Car(String model, String color) {
+//		this.model = model;
+//		this.color = color;
+		this(model, color, 250);
+	}
+
+	// 생성자를 명시하지 않으면 컴파일 과정에서 생성자를 생성해준다.
+	Car(String model, String color, int maxSpeed) {
+		this.model = model;
+		this.color = color;
+		this.maxSpeed = maxSpeed;
+
+	}
+
+}
